5a31f458f912afc7e796643bfcb322b0f305c0f5
EricCRodriguez <ecrod12@gmail.com>
2021-01-18 16:49:43 -0600

    input functions setup and working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([
+    " Walk the puppies",
+    "Feed the puppies",
+    "dont forget to fast at 6pm",
+  ]);
+  const [input, setInput] = useState("");
+  console.log("console", input);
+
+  const addTodo = (event) => {
+    //this will fire off when we click the button
+    console.log("console", "Im Working!!!");
+    setTodos([...todos, input]);
+  };
+
   return (
     <div className="App">
       <h1>Yo</h1>
+      <input value={input} onChange={(event) => setInput(event.target.value)} />
+      <button onClick={addTodo}>Add Todo</button>
+
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
